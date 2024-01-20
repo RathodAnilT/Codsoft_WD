edc74ef356966ca49e890e2932d260433e72f0ab
RathodAnilT <132031578+RathodAnilT@users.noreply.github.com>
2024-01-20 20:39:36 +0530

    quiz app

---
+++ quiz_app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import React from 'react'
+import Quiz from './components/Quiz';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
